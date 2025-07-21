7777dc9e15ef9fefb0a73d04753ffd697ac063c8
dinhthanh143 <neonlimea@gmail.com>
2025-07-21 20:53:50 +0700

    Add files via upload

---
+++ PTIT_CNTT1_IT201_Session19/PTIT_CNTT1_IT201_Session19_Bai06.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+typedef struct Queue {
+    Node **arr;
+    int front;
+    int rear;
+    int cap;
+} Queue;
+
+Node *createNode(int data) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+Queue *createQueue(int cap) {
+    Queue *q = (Queue*)malloc(sizeof(Queue));
+    q->arr = (Node**)malloc(sizeof(Node*) * cap);
+    q->front = 0;
+    q->rear = -1;
+    q->cap = cap;
+    return q;
+}
+
+int isEmpty(Queue *q) {
+    return q->front > q->rear;
+}
+int isFull(Queue *q) {
+    return q->rear == q->cap - 1;
+}
+
+void enqueue(Queue *q, Node *node) {
+    if (isFull(q)) return;
+    q->arr[++q->rear] = node;
+}
+
+
+void BFS(Node *root, int value) {
+    if (root == NULL) {
+        printf("FALSE\n");
+        return;
+    }
+    Queue *q = createQueue(100);
+    enqueue(q, root);
+    while (!isEmpty(q)) {
+        Node *node = q->arr[q->front++];
+        if (node->data == value) {
+            printf("TRUE\n");
+            return;
+        }
+        if (node->left)  enqueue(q, node->left);
+        if (node->right) enqueue(q, node->right);
+    }
+    printf("FALSE\n");
+}
+
+int main() {
+    Node *root = createNode(2);
+    root->left = createNode(3);
+    root->right = createNode(4);
+    root->left->left = createNode(5);
+
+    int value;
+    printf("Moi nhap gia tri muon tim: ");
+    scanf("%d", &value);
+
+    BFS(root, value);
+
+    return 0;
+}
---
+++ PTIT_CNTT1_IT201_Session19/PTIT_CNTT1_IT201_Session19_Bai08.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stdbool.h>
+typedef struct Node {
+   int data;
+   struct Node *left;
+   struct Node *right;
+} Node;
+
+Node *createNode(int data) {
+   Node *node = (Node *)malloc(sizeof(Node));
+   node->data = data;
+   node->left = NULL;
+   node->right = NULL;
+   return node;
+}
+void printTree(Node *root) {
+   if (root == NULL) {
+      return;
+   }
+   printTree(root->left);
+   printf("%d \t", root->data);
+   printTree(root->right);
+}
+int getHeight(Node *root) {
+   if (root == NULL) {
+      return -1;
+   }
+   int left = getHeight(root->left);
+   int right = getHeight(root->right);
+   if (left > right) {
+      return left +1;
+   }else {
+      return right +1;
+   }
+}
+int main(){
+   Node *root = createNode(2);
+   root->left = createNode(3);
+   root->right = createNode(4);
+   root->left->left = createNode(5);
+   // root->left->left->left = createNode(6);
+   printTree(root);
+   printf("\n%d", getHeight(root));
+   
+   return 0;
+}
